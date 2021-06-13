fa82f074f95e9928bb5e8667950fd6ecd485d65e
MoSaif00 <69274977+MoSaif00@users.noreply.github.com>
2021-06-14 01:28:47 +0200

    background to white for shadow

--- technical-assignments/w3-w4/Assignment-DatHuis/src/components/styled/appContainer.style.js
+++ technical-assignments/w3-w4/Assignment-DatHuis/src/components/styled/appContainer.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   padding-top: 15vh;
-  background-color: wheat;
+  background-color: white;
   width: 100vw;
   height: 100vh;
   position: relative;
